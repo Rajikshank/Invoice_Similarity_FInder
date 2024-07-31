412af4ed0dc86ff7351a0ae209331683916e1f55
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2024-07-31 17:18:14 +0100

    app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from dotenv import load_dotenv
-load_dotenv()
-from Db import *
 
+from Db import *
+from Helper import *
 import streamlit as st
 import os
 
@@ -14,19 +13,19 @@ def main():
     if uploaded_file is not None:
         # Extract and display text from uploaded file
         text = extract_text_from_pdf(uploaded_file)
-        st.write("Extracted Text:")
-        st.write(text)
+        #st.write("Extracted Text:")
+        #st.write(text)
         
         # Extract features and find the most similar invoice
         features = extract_features(text)
         
         # Example existing invoices
         database = InvoiceDatabase()
-        database.add_invoice('invoice1.pdf', extract_features(extract_text_from_pdf(open('train\2024.03.15_0954.pdf', 'rb'))))
-        database.add_invoice('invoice2.pdf', extract_features(extract_text_from_pdf(open('train\2024.03.15_1145.pdf', 'rb'))))
-        database.add_invoice('invoice2.pdf', extract_features(extract_text_from_pdf(open('train\Faller_8.PDF', 'rb'))))
-        database.add_invoice('invoice2.pdf', extract_features(extract_text_from_pdf(open('train\invoice_77073.pdf', 'rb'))))
-        database.add_invoice('invoice2.pdf', extract_features(extract_text_from_pdf(open('train\invoice_102856.pdf', 'rb'))))
+        database.add_invoice('invoice 1 - Bremer Spirituosen Contor GmbH', extract_features(extract_text_from_pdf(open('./train/2024.03.15_0954.pdf', 'rb'))))
+        database.add_invoice('invoice 2 - Bremer Spirituosen Contor GmbH', extract_features(extract_text_from_pdf(open('./train/2024.03.15_1145.pdf', 'rb'))))
+        database.add_invoice('invoice 3 - Faller', extract_features(extract_text_from_pdf(open('./train/Faller_8.PDF', 'rb'))))
+        database.add_invoice('invoice 4 - Süderelbe HAF Catering u. Service GmbH', extract_features(extract_text_from_pdf(open('../train/invoice_77073.pdf', 'rb'))))
+        database.add_invoice('invoice 5 - GmbH & Co', extract_features(extract_text_from_pdf(open('./train/invoice_102856.pdf', 'rb'))))
         
         most_similar_file, similarity_score = database.find_most_similar(features)
         st.write(f'The most similar invoice is: {most_similar_file}')
